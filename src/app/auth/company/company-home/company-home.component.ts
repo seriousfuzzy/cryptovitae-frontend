03ae29102e8e53534ba5d9d887abdee6eb8ce8fd
williamddobson3 <williamddobson3@gmail.com>
2024-11-15 09:16:35 +0900

    feat: added sidebar & navbar to company-home route
    Co-authored-by: seriousfuzzy <seriousfuzzy1211@gmail.com>
    Co-authored-by: williamddobson3 <williamddobson3@gmail.com>

--- src/app/auth/company/company-home/company-home.component.ts
+++ src/app/auth/company/company-home/company-home.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-company-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './company-home.component.html',
   styleUrl: './company-home.component.css'
 })
 export class CompanyHomeComponent {
+  constructor(private router: Router) {}
 
+  // Aca agregan las rutas que necesiten porfa
+  sidebarItems = [
+    {
+      name: 'Home',
+      icon: 'pi-briefcase',
+      active: false,
+      route: '/app/manage-projects',
+    },
+    {
+      name: 'Ver empleados',
+      icon: 'pi-search-plus',
+      active: false,
+      route: '/app/my-donations',
+    },
+    {
+      name: 'Buscar prospectos',
+      icon: 'pi-search',
+      active: false,
+      route: '/app/explore-projects',
+    },
+  ];
+
+  ngOnInit(): void {
+    this.selectItem(this.sidebarItems[0]);
+  }
+
+  selectItem(item: any) {
+    this.sidebarItems.forEach((i) => (i.active = false));
+    item.active = true;
+
+    this.router.navigate([item.route]);
+  }
+
+  logout(): void {}
 }
