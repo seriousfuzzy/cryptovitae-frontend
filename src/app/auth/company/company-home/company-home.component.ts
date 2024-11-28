86a76da660c15b504f7401f84d397e3fab8f6fc7
williamddobson3 <williamddobson3@gmail.com>
2024-11-28 11:20:01 +0900

    feat: added companies component
    Co-authored-by: seriousfuzzy <seriousfuzzy1211@gmail.com>
    Co-authored-by: williamddobson3 <williamddobson3@gmail.com>

--- src/app/auth/company/company-home/company-home.component.ts
+++ src/app/auth/company/company-home/company-home.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-company-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './company-home.component.html',
   styleUrl: './company-home.component.css'
 })
 export class CompanyHomeComponent {
+  constructor(private router: Router) {}
 
+  // Aca agregan las rutas que necesiten porfa
+  sidebarItems = [
+    {
+      name: 'Mis empresas',
+      icon: 'pi-briefcase',
+      active: false,
+      route: '/auth/company/my-companies',
+    },
+    {
+      name: 'Ver empleados',
+      icon: 'pi-search-plus',
+      active: false,
+      route: '/auth/company/employees',
+    },
+    {
+      name: 'Buscar prospectos',
+      icon: 'pi-search',
+      active: false,
+      route: '/auth/company/prospects',
+    },
+  ];
+
+  ngOnInit(): void {
+    this.selectItem(this.sidebarItems[0]);
+  }
+
+  selectItem(item: any) {
+    this.sidebarItems.forEach((i) => (i.active = false));
+    item.active = true;
+
+    this.router.navigate([item.route]);
+  }
+
+  logout(): void {}
 }
--- src/app/landing/login/login.component.ts
+++ src/app/landing/login/login.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { MetaMaskService } from '../../services/metamask.service';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    DividerModule,
+    ToggleButtonModule,
+    ReactiveFormsModule,
+    ToastModule,
+    RadioButtonModule,
+    DropdownModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  accountAddress: string | null = null;
+  categoryControl: FormControl = new FormControl('', Validators.required);
 
+  categories: any[] = [
+    { name: 'Personal', code: 'personal' },
+    { name: 'Empresa', code: 'empresa' },
+  ];
+
+  constructor(
+    private router: Router,
+    private messageService: MessageService,
+    private metaMaskService: MetaMaskService
+  ) {}
+
+  async login(): Promise<void> {
+    await this.metaMaskService.connectMetaMask().then((accountAddress) => {
+      if (!accountAddress) {
+        console.error('Failed to connect to MetaMask');
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Failed to connect to MetaMask',
+          life: 2000,
+        });
+        return;
+      }
+
+      this.accountAddress = accountAddress;
+      console.log('Connected account:', this.accountAddress);
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Logged in successfully',
+        life: 2000,
+      });
+      setTimeout(() => {
+        if (this.categoryControl.value.code === 'empresa') {
+          this.router.navigate(['/auth/company']);
+          return;
+        } else {
+          this.router.navigate(['/auth/personal']);
+        }
+      }, 2000);
+    });
+  }
 }
