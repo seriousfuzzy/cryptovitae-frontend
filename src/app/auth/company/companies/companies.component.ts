00deead07a3a7cb037cf4c048e8fa25c76625bf2
seriousfuzzy <seriousfuzzy1211@gmail.com>
2024-12-12 19:15:11 +0900

    feat: upload nft to pinata poc

--- src/app/auth/company/companies/companies.component.ts
+++ src/app/auth/company/companies/companies.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { PinataService } from '../../../../services/pinata.service';
 
 @Component({
   selector: 'app-companies',
@@ -12,5 +13,27 @@ import { CardModule } from 'primeng/card';
   styleUrl: './companies.component.css'
 })
 export class CompaniesComponent {
+  imageFile: File | null = null;
+  tokenUri: string | null = null;
 
+  constructor(private pinataService: PinataService) {}
+
+  onImageSelected(event: any) {
+    this.imageFile = event.target.files[0];
+  }
+
+  uploadNft() {
+    if (this.imageFile) {
+      this.pinataService.uploadNft(this.imageFile, {}).subscribe((result) => {
+        console.log('Image CID:', result.imageCid);
+        console.log('JSON CID:', result.jsonCid);
+        this.tokenUri = result.tokenUri;
+        console.log('Token URI:', this.tokenUri);
+
+        // call contract
+      });
+    } else {
+      console.error('No image file selected.');
+    }
+  }
 }
