bcf0bfbabc40372edd29bbe8f660890c66929e5d
billmbowling3 <billmbowling3@gmail.com>
2024-12-17 09:19:39 +0900

    update: company info component

--- src/app/auth/company/companies/companies.component.ts
+++ src/app/auth/company/companies/companies.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { PinataService } from '../../../../services/pinata.service';
 
 @Component({
   selector: 'app-companies',
@@ -13,30 +12,8 @@ import { PinataService } from '../../../../services/pinata.service';
   styleUrl: './companies.component.css',
 })
 export class CompaniesComponent {
-  imageFile: File | null = null;
-  tokenUri: string | null = null;
+  constructor(private router: Router) {}
 
-  constructor(private router: Router, private pinataService: PinataService) {}
-
-  onImageSelected(event: any) {
-    this.imageFile = event.target.files[0];
-  }
-
-  uploadNft() {
-    if (this.imageFile) {
-      this.pinataService.uploadNft(this.imageFile, {}).subscribe((result) => {
-        console.log('Image CID:', result.imageCid);
-        console.log('JSON CID:', result.jsonCid);
-        this.tokenUri = result.tokenUri;
-        console.log('Token URI:', this.tokenUri);
-
-        // call contract
-      });
-    } else {
-      console.error('No image file selected.');
-    }
-  }
-  
   goToCompanyInfo() {
     this.router.navigate(['/auth/company/company-info']);
   }
