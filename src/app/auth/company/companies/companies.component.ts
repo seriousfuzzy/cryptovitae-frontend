7fa699192e127a98d703e872c10d371709319966
billmbowling3 <billmbowling3@gmail.com>
2024-12-26 18:19:40 +0900

    Merge branch 'company-information' of github.com-personal:Crypto-Vitae/frontend into contracts

--- src/app/auth/company/companies/companies.component.ts
+++ src/app/auth/company/companies/companies.component.ts
@@ -1,25 +1,68 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 import { PinataService } from '../../../services/pinata.service';
 
 @Component({
   selector: 'app-companies',
   standalone: true,
-  imports: [ButtonModule, CommonModule, RouterModule, CardModule],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    CardModule,
+    DialogModule,
+    FileUploadModule,
+    InputTextModule,
+    DropdownModule,
+  ],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.css',
 })
 export class CompaniesComponent {
+  companyForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+  });
+  dialogVisible = false;
   imageFile: File | null = null;
   tokenUri: string | null = null;
+
+  categories: any[] = [
+    { name: 'Desarrollo de software', code: 'desarrollo-software' },
+    { name: 'Marketing', code: 'marketing' },
+    { name: 'Recursos Humanos', code: 'recursos-humanos' },
+    { name: 'Finanzas', code: 'finanzas' },
+    { name: 'IT e Infraestructura', code: 'it-e-infraestructura' },
+    { name: 'Cadena de Suministro', code: 'cadena-de-suministro' },
+    { name: 'Gestión de Producto', code: 'gestion-de-producto' },
+    { name: 'Desarrollo de Negocios', code: 'desarrollo-de-negocios' },
+    { name: 'Relaciones Públicas', code: 'relaciones-publicas' },
+    {
+      name: 'Aseguramiento de la Calidad',
+      code: 'aseguramiento-de-la-calidad',
+    },
+  ];
 
   constructor(private router: Router, private pinataService: PinataService) {}
 
   onImageSelected(event: any) {
-    this.imageFile = event.target.files[0];
+    console.log('Image selected:', event);
+    this.imageFile = event.currentFiles[0];
   }
 
   uploadNft() {
@@ -36,8 +79,18 @@ export class CompaniesComponent {
       console.error('No image file selected.');
     }
   }
-  
+
   goToCompanyInfo() {
     this.router.navigate(['/auth/company/company-info']);
+  }
+
+  createCompany() {
+    if (this.companyForm.valid) {
+      console.log('Company form:', this.companyForm.value);
+      this.dialogVisible = false;
+
+      this.uploadNft();
+      // call contract
+    }
   }
 }
